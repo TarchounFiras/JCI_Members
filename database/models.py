894837c6b701b0b69ddf8cbd26d3d0da3a88a130
tarchoun_firas <tarchounfiras@gmail.com>
2025-01-23 09:28:11 +0100

    correct achievement field doesnt support list

--- database/models.py
+++ database/models.py
@@ -1,17 +1,17 @@
 from typing import List
-from sqlmodel import Field ,Relationship,SQLModel
+from sqlmodel import Field ,SQLModel
 from datetime import date
 
 
 class Member(SQLModel, table=True):
-    id:int=Field(primary=True)
+    id:int=Field(primary_key=True)
     name:str=Field()
     birthday:date|None=Field(default=None)
     email:str=Field(unique=True,index=True)
     hashed_password:str=Field()
     points:int=Field(default=0)
     avatar:str|None=Field(default=None)
-    achievements:List[str]=Field(default=[])
+    achievements:str=Field(default="")
     presence:int=Field(default=0)
     trend:str=Field(default="stable")
     rank:int=Field(default=0)
